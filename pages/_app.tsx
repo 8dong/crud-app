969fc9f568bf76af34ed0f27d545add52f520430
DongHyun <hyde981114@gmail.com>
2022-12-09 02:03:52 +0900

    feat: ModalContextProvider 컴포넌트 추가
    modal 상태와 핸들러를 context API로 관리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,15 +9,18 @@ import store, { persistor } from '../redux/store/store';
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import ModalContextProvider from '../context/modal/ModalContextProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <PageLayout>
-          <MainHeader />
-          <Component {...pageProps} />
-        </PageLayout>
+        <ModalContextProvider>
+          <PageLayout>
+            <MainHeader />
+            <Component {...pageProps} />
+          </PageLayout>
+        </ModalContextProvider>
       </PersistGate>
     </Provider>
   );
