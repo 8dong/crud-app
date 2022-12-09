80d77b9dcb8b9ea101262252bfb5f62d5efebf19
DongHyun <hyde981114@gmail.com>
2022-12-09 15:21:13 +0900

    feat: DataListSection react-transition-group 적용
    data 추가, 삭제될 때의 Animation을 적용하기 위해 react-transition-group을 적용

--- components/UI/organisms/DataListSection.tsx
+++ components/UI/organisms/DataListSection.tsx
@@ -1,4 +1,6 @@
 import { useSelector } from 'react-redux';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
@@ -21,13 +23,15 @@ const DataListSection = () => {
   return (
     <DataListSectionWrapper>
       <h2>Data List</h2>
-      <ul>
+      <TransitionGroup component='ul'>
         {newestDataList.map((data) => (
-          <li key={data.id} onClick={handleClickDataItem(data.id)}>
-            <DataItem data={data} />
-          </li>
+          <CSSTransition key={data.id} timeout={500} classNames='fade' mountOnEnter unmountOnExit>
+            <li onClick={handleClickDataItem(data.id)}>
+              <DataItem data={data} />
+            </li>
+          </CSSTransition>
         ))}
-      </ul>
+      </TransitionGroup>
     </DataListSectionWrapper>
   );
 };
@@ -38,6 +42,46 @@ const DataListSectionWrapper = styled.section`
   h2,
   li {
     margin-bottom: 20px;
+  }
+
+  .fade-enter-active {
+    animation: add-item 500ms;
+  }
+
+  .fade-exit-active {
+    animation: del-item 500ms;
+  }
+
+  @keyframes add-item {
+    0% {
+      opacity: 0;
+    }
+
+    30% {
+      opacity: 0.5;
+      transform: translateY(5px);
+    }
+
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes del-item {
+    0% {
+      opacity: 1;
+    }
+
+    30% {
+      opacity: 0.5;
+      transform: translateY(5px);
+    }
+
+    100% {
+      opacity: 0;
+      transform: translateY(0);
+    }
   }
 `;
 
